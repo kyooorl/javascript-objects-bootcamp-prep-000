4f5390c2b105b6b6882f764b2eec0d03b7f6f553
Monica Lee <mylee.715@gmail.com>
2019-06-01 22:10:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,8 +3,3 @@ var playlist = new Object ({ artistName: 'songTitle' });
  function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { artistName: [songTitle]});
 }
-
- function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  return playlist;
-}
